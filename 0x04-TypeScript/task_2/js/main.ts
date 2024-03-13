b8c4785ea312f777988672671a399fef91a6fc30
Linetgithub <awinoojwang22@gmail.com>
2024-03-13 15:36:21 +0300

    typescript

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,16 @@
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    throw new Error('Invalid class');
+  }
+}
+
+// Test cases
+console.log(teachClass('Math'));     // Output: Teaching Math
+console.log(teachClass('History'));  // Output: Teaching History
+
